202cc92def87469b33b9bc813da1b0c431d31d3a
atp <pruthvip15@gmail.com>
2018-10-17 12:38:49 +0530

    Fix for when the component is unmounted quickly
    Fix for when the component is unmounted quickly. the callback of the setstate in componentDidMount is called after the componentWillUnmount, hence the componentWillUnmount results in an error.

--- packages/modal-react-native-web/src/Portal.js
+++ packages/modal-react-native-web/src/Portal.js
@@ -22,7 +22,7 @@ export default class Portal extends Component {
   }
 
   componentWillUnmount() {
-    this.state.target.removeChild(this.state.el);
+    this.state.target && this.state.target.removeChild(this.state.el);
   }
 
   render() {
